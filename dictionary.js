4f8a66f63a5afc83606a3ab02a1c1aeca952a9d2
Yigit ozcelep <yoroso85onurlu@outlook.com>
2024-05-12 21:59:16 +0300

    add langauge support

--- dictionary.js
+++ dictionary.js
@@ -3,21 +3,23 @@ const languages = {
     english: "english",
 }
 
+if (!localStorage.getItem("language")) localStorage.setItem("language", languages.turkish);
 
-if (localStorage.getItem("language")) localStorage.setItem("language") = languages.turkish;
 
 const dictionary = {
     "login": {
         [languages.turkish]: "giriş",
         [languages.english]: "login",
     },
-
+    
     "password": {
         [languages.turkish]: "şifre",
         [languages.english]: "password",
-    }
-    
+    },
 }
 
+const getText = (text) => {
+    return dictionary[text][localStorage.getItem("language")];
+}
 
-export {dictionary}
+export { getText, languages };

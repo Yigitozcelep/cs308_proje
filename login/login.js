c59a1ec15a0cef337e2095c2ab9af599c45667a2
dogaturak <dogaturak@sabanciuniv.edu>
2024-05-23 01:55:59 +0300

    create member

--- login/login.js
+++ login/login.js
@@ -62,6 +62,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
+dummyUsers[0].userType = "passenger";
 document.querySelector('form').addEventListener('submit', async function(event){
     // Prevent the default form submission behavior
     event.preventDefault();

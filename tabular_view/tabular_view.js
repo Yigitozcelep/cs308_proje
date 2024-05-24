2ba2da6d27273c54a4f36bd3a258cd9b1539dd95
sudetaskin <116296583+sudetaskin@users.noreply.github.com>
2024-05-24 16:45:30 +0300

    Update tabular_view.js

--- tabular_view/tabular_view.js
+++ tabular_view/tabular_view.js
@@ -1,10 +1,9 @@
 import { FlightsCommunication } from "../backend_communication/flights/flights_communication.js";
-import { dummyFlights, dummyUsers } from "../backend_communication/dummy_data.js";
 import { UserCommunication } from "../backend_communication/users/users_communication.js";
-import { getText } from "../dictionary.js";
 import { FlightData } from "../backend_communication/flights/flights.js";
+import { getText } from "../dictionary.js";
 
-
+const currentState = document.getElementById("table-type").innerHTML;
 
 function handleLanguageChange() {
     let lang = document.getElementById('language').value;
@@ -13,98 +12,97 @@ function handleLanguageChange() {
     const logo = document.querySelector('.logo');
     const apply = document.getElementById('apply');
     const helpButton = document.getElementById('helpButton');
-    const signOutButton = document.querySelector('.top-bar-item[href="signout.php"]');
-
+   // const signOutButton = document.querySelector('.top-bar-item[href="signout.php"]');
 
     apply.innerHTML = getText("apply");
     logo.innerHTML = getText("AIR308 Airlines");
     helpButton.innerHTML = getText("helpButton");
-    signOutButton.innerHTML = getText("signOutButton");
+    //signOutButton.innerHTML = getText("signOutLink");
 
-    document.getElementById('name_search').setAttribute('placeholder', getText("name_search"));
-    document.getElementById('surname_search').setAttribute('placeholder', getText("surname_search"));
-    document.getElementById('id_search').setAttribute('placeholder', getText("id_search"));
-    document.getElementById('email_search').setAttribute('placeholder', getText("email_search"));
-    document.getElementById('seat_search').setAttribute('placeholder', getText("seat_search"));
+    /*document.getElementById('name-search').setAttribute('placeholder', getText("name-search"));
+    document.getElementById('surname-search').setAttribute('placeholder', getText("surname-search"));
+    document.getElementById('id-search').setAttribute('placeholder', getText("id-search"));
+    document.getElementById('email-search').setAttribute('placeholder', getText("email-search"));
+    document.getElementById('seat-search').setAttribute('placeholder', getText("seat-search"));
     document.getElementById('name-tabular').setAttribute('placeholder', getText("name-tabular"));
-
+    */
 }
 
 document.addEventListener('DOMContentLoaded', handleLanguageChange);
-
-// Add event listener to the language dropdown to handle language change
 document.getElementById('language').addEventListener('change', handleLanguageChange);
 
-
-document.addEventListener('DOMContentLoaded', handleLanguageChange);
-
-// Add event listener to the language dropdown to handle language change
-document.getElementById('language').addEventListener('change', handleLanguageChange);
-console.log(localStorage.getItem('flightIdView'));
-//console.log(dummyUsers);
 document.addEventListener('DOMContentLoaded', async function () {
+
+    let FlightData = await FlightsCommunication.getFlightByFlightId(localStorage.getItem("flightIdView"));
+    const currentState = document.getElementById("table-type").innerHTML;
+    let currentData;
+    if (currentState === "Pilot Crew") currentData = await FlightsCommunication.getPilotData(FlightData);
+    else if (currentState === "Passenger") currentData = await FlightsCommunication.getPassangerData(FlightData);
+    else if (currentState === "Cabin Crew") currentData = await FlightsCommunication.getFlightCrew(FlightData);
+
     console.log(localStorage.getItem("flightIdView"));
     let flightData = await FlightsCommunication.getFlightByFlightId(localStorage.getItem("flightIdView"));
     console.log(flightData);
     let userData = await FlightsCommunication.getFlightCrew(flightData);
+    const x = await FlightsCommunication.getPassangerData(flightData);
+
+    userData
     var state = {
         'querySet': userData,
         'page': 1,
         'rows': 20,
-        'currentTable': 'Cabin Crew'
+        'currentTable': currentState
     };
+    console.log(userData);
 
     async function getUserRole() {
         const userId = localStorage.getItem("userId");
         const user = await UserCommunication.getUserById(userId);
-        user.userType = "admin" //datadan dolayı burası dynamically gelmeli
+        user.userType = "Admin"; // Datadan dolayı burası dynamically gelmeli
         console.log(user.userType);
         return user.userType;
     }
 
-
     async function showProfileIcon() {
         const userRole = await getUserRole();
         const profileIcon = document.getElementById('profileIcon');
-        if (userRole === 'admin') {
+        if (userRole === 'Admin') {
             profileIcon.style.display = 'none';
         } else {
             profileIcon.style.display = 'block';
         }
     }
-    
+
+    function filterByUserType(userType, userData) {
+        return userData.filter(user => user.userType === userType);
+    }
+
     function buildTable() {
         var data = pagination(state.querySet, state.page, state.rows);
-        var tableBody = document.querySelector(`#${state.currentTable.toLowerCase().replace(' ', '-')}-table tbody`);
+        var tableBody = document.querySelector(`#table-body-${state.currentTable.toLowerCase().replace(' ', '-')}`);
         tableBody.innerHTML = '';
 
-
-
-
-            data.querySet.forEach(function (item) {
-                let seatNo = '';
-                if (item.flights && item.flights.length > 0) {
-                    for (let item1 of item.flights) {
-                        const flightData = item1.flightData;
-                       // console.log('Flight ID:', flightData.getFlightId());
-                        if (flightData.getFlightId() == localStorage.getItem("flightIdView")) {
-                            seatNo = item1.userSeat.getSeatPosition();
-                            console.log('Seat Position:', seatNo);
-                            console.log('userData');
-                            break;
-                        }
+        data.querySet.forEach(function (item) {
+            let seatNo = '';
+            if (item.flights && item.flights.length > 0) {
+                for (let item1 of item.flights) {
+                    const flightData = item1.flightData;
+                    if (flightData.getFlightId() == localStorage.getItem("flightIdView")) {
+                        seatNo = item1.userSeat.getSeatPosition();
+                        break;
                     }
                 }
-                
-                var row = `<tr>
-                    <td>${item.name}</td>
-                    <td>${item.surname}</td>
-                    <td>${item.Id}</td>
-                    <td>${item.email}</td>
-                    <td>${seatNo ? seatNo : 'N/A'}</td>
-                </tr>`;
-                tableBody.innerHTML += row;
-            });
+            }
+
+            var row = `<tr>
+                <td>${item.name}</td>
+                <td>${item.surname}</td>
+                <td>${item.Id}</td>
+                <td>${item.email}</td>
+                <td>${seatNo ? seatNo : 'N/A'}</td>
+            </tr>`;
+            tableBody.innerHTML += row;
+        });
 
         pageButtons(data.pages);
     }
@@ -137,6 +135,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         return function innerSort(a, b) {
             const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
             const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+
             let comparison = 0;
             if (varA > varB) {
                 comparison = 1;
@@ -165,11 +164,11 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     function searchTable(filters, data) {
         return data.filter(item => {
-            return (!filters.name || item.Name.toLowerCase().includes(filters.name.toLowerCase())) &&
-                (!filters.surname || item.Surname.toLowerCase().includes(filters.surname.toLowerCase())) &&
-                (!filters.id || item.CrewID.toLowerCase().includes(filters.id.toLowerCase())) &&
-                (!filters.email || item.Email.toLowerCase().includes(filters.email.toLowerCase())) &&
-                (!filters.seatNum || item.SeatNum.toLowerCase().includes(filters.seatNum.toLowerCase()));
+            return (!filters.name || item.name.toLowerCase().includes(filters.name.toLowerCase())) &&
+                (!filters.surname || item.surname.toLowerCase().includes(filters.surname.toLowerCase())) &&
+                (!filters.id || item.Id.toLowerCase().includes(filters.id.toLowerCase())) &&
+                (!filters.email || item.email.toLowerCase().includes(filters.email.toLowerCase())) &&
+                (!filters.seatNum || item.seatNum.toLowerCase().includes(filters.seatNum.toLowerCase()));
         });
     }
 
@@ -182,10 +181,17 @@ document.addEventListener('DOMContentLoaded', async function () {
             seatNum: document.getElementById('seat-search').value
         };
         const currentState = document.getElementById("table-type").innerHTML;
+
         let currentData;
-        if (currentState === "Pilot Crew") currentData = FlightsCommunication.getPilotData(currentFlight);
-        else if (currentState === "Passenger") currentData = FlightsCommunication.getPassangerData(currentFlight);
-        else if (currentState === "Cabin Crew") currentData = FlightsCommunication.getFlightCrew(currentFlight);
+        if (currentState === "Pilot Crew") {
+            currentData = FlightsCommunication.getFlightCrew()
+            currentData = filterByUserType(currentState, userData);
+        } else if (currentState === "Passenger") {
+            currentData = filterByUserType(currentState, userData);
+        } else if (currentState === "Cabin Crew") {
+            currentData = filterByUserType(currentState, userData);
+        }
+
         var filteredData = searchTable(filters, currentData);
         state.querySet = filteredData;
         state.page = 1;
@@ -206,19 +212,19 @@ document.addEventListener('DOMContentLoaded', async function () {
         document.getElementById('cabin-crew-table').style.display = 'none';
         document.getElementById('passenger-table').style.display = 'none';
         document.getElementById('pilot-crew-table').style.display = 'none';
-
-        if (userRole === 'admin') {
-            document.getElementById(`${tableName.toLowerCase().replace(' ', '-')}-table`).style.display = 'block';
-        } else if (userRole === 'pilot' && tableName !== 'Passenger') {
-            document.getElementById(`${tableName.toLowerCase().replace(' ', '-')}-table`).style.display = 'block';
-        } else if (userRole === 'cabincrew' && tableName !== 'Pilot Crew') {
-            document.getElementById(`${tableName.toLowerCase().replace(' ', '-')}-table`).style.display = 'block';
-        } else {
-            return; 
+        if (tableName === 'Passenger') {
+            console.log(userData)
+            state.querySet = filterByUserType("Passsanger", userData);
+        } else if (tableName === 'Cabin Crew') {
+            state.querySet = filterByUserType("CabinCrew", userData);
+        } else if (tableName === 'Pilot Crew') {
+            state.querySet = filterByUserType("PilotCrew", userData);
         }
 
+        document.getElementById(`${tableName.toLowerCase().replace(' ', '-')}-table`).style.display = 'block';
         state.currentTable = tableName;
         tableType.textContent = tableName;
+        console.log(tableName);
         buildTable();
         updateButtons(tableName, userRole);
     }
@@ -259,11 +265,10 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     showTable('Cabin Crew');
     showProfileIcon();
-
+    console.log(currentState);
 
     document.getElementById('signOutLink').addEventListener('click', function(event) {
         event.preventDefault();
-        window.location.href = '../start_screen/index.html';
+        window.location.shref = '../start_screen/index.html';
     });
 });
-
